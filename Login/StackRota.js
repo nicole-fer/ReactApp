2c4e4c90c4ef82eb81c03bb9df44571793a2983c
nicole-fer <nicolefernandes@alu.ufc.br>
2022-07-18 10:23:14 -0300

    Personalizando tab bar

--- Login/StackRota.js
+++ Login/StackRota.js
@@ -2,13 +2,8 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import PageOne from '../PageOne/PageOne';
-import PageTwo from '../PageTwo/PageTwo';
 import Login from '../Login/Login';
 import twn from '../Tailwind';
-import Agenda from '../PageTwo/Agenda';
-import Consultas from '../PageTwo/Consultas';
-import Informacoes from '../PageTwo/Informacoes';
-import Perfil from '../PageTwo/Perfil';
 import Preload from './Preload/Preload';
 import Rota from '../PageTwo/Rota';
 
@@ -18,7 +13,7 @@ const UselessTextInput = () => {
     return (
         <Stack.Navigator             
             initialRouteName = "Preload">
-            <Stack.Screen
+{/*             <Stack.Screen
                 options={{
                     headerShown: false,
                 }}
@@ -26,8 +21,8 @@ const UselessTextInput = () => {
                 component = {Preload}
 
             >    
-            </Stack.Screen>
-            <Stack.Screen 
+            </Stack.Screen> */}
+{/*             <Stack.Screen 
                 name="Login"
                 component={Login}
                 signup = {false} 
@@ -37,7 +32,7 @@ const UselessTextInput = () => {
                     headerTitleStyle: twn`text-white`,
                 }}
                 //onSubmit={home}
-            /> 
+            />  */}
             <Stack.Screen 
                 name="Rota" 
                 component={Rota}
@@ -53,58 +48,9 @@ const UselessTextInput = () => {
                     headerTitleStyle: twn`text-white`,
                 }}
                 /> 
-{/*             <Stack.Screen 
-                name="Página Inicial" 
-                component={PageTwo}
-            />  */}
         </Stack.Navigator>
     );
 };
-/* 
-const MenuStackPageTwo = () => {
-    <Stack.Navigator>            
-        <Stack.Screen 
-            name="Página Inicial" 
-            component={PageTwo}
-            options={{header: () => null}}
-        /> 
-    </Stack.Navigator>
-} */
-
-/* const MenuStackConsultas = () => {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen 
-                name="Consutas"
-                component={Consultas} 
-                options={{header: () => null}} />
-        </Stack.Navigator>
-    )
-}
-
-const MenuStackAgenda = () => {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen name="Agenda" component={Agenda} options={{header: () => null}}  />
-        </Stack.Navigator>
-    )
-}
-
-const MenuStackInfo = () => {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen name="Informações" component={Informacoes} options={{header: () => null}}  />
-        </Stack.Navigator>
-    )
-}
-
-const MenuStackPerfil = () => {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen name="Perfil" component={Perfil} options={{header: () => null}} />
-        </Stack.Navigator>
-    )
-} */
 
 
 export default UselessTextInput;
--- PageTwo/Rota.js
+++ PageTwo/Rota.js
@@ -5,14 +5,63 @@ import Perfil from './Perfil';
 import Agenda from './Agenda';
 import Consutas from './Consultas';
 import PageTwo from './PageTwo';
+import twn from '../Tailwind';
+import { Entypo, Feather, AntDesign, MaterialCommunityIcons } from '@expo/vector-icons'
 
 const Tab = createBottomTabNavigator();
 export default  () => (
-    <Tab.Navigator>
-        <Tab.Screen name="Página Inicial" component={PageTwo}/> 
-        <Tab.Screen name="Consutas" component={Consutas} />
-        <Tab.Screen name="Agenda" component={Agenda} />
-        <Tab.Screen name="Informações" component={Informacoes} />
-        <Tab.Screen name="Perfil" component={Perfil} />
+    <Tab.Navigator 
+        screenOptions={{
+            tabBarStyle:twn`bg-teal-300 py-1`,
+            tabBarActiveTintColor: '#a21caf',
+            tabBarInactiveTintColor: '#0f766e'
+        }
+    }
+    >
+        <Tab.Screen 
+            name="Página Inicial" 
+            component={PageTwo}
+            options={{
+                tabBarIcon: ({ size, color }) => (
+                    <Entypo name="home" size={size} color={color} />
+                )
+            }}
+        /> 
+        <Tab.Screen 
+            name="Consutas"
+            component={Consutas}
+            options={{
+                tabBarIcon: ({ size, color }) => (
+                    <AntDesign name="solution1" size={size} color={color} />
+                )
+            }} 
+        />
+        <Tab.Screen 
+            name="Agenda"
+            component={Agenda}
+            options={{
+                tabBarIcon: ({ size, color }) => (
+                    <MaterialCommunityIcons name="calendar-month" size={size} color={color} />
+                )
+            }} 
+         />
+        <Tab.Screen 
+            name="Informações"
+            component={Informacoes} 
+            options={{
+                tabBarIcon: ({ size, color }) => (
+                    <AntDesign name="form" size={size} color={color} />
+                )
+            }} 
+        />
+        <Tab.Screen 
+            name="Perfil"
+            component={Perfil}
+            options={{
+                tabBarIcon: ({ size, color }) => (
+                    <MaterialCommunityIcons name="face-woman" size={size} color={color} />
+                )
+            }} 
+         />
     </Tab.Navigator>
 );
