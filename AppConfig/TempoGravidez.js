0e56db70e02bcdcd405303cad3170f5d47e0cf47
nicole-fer <nicolefernandes@alu.ufc.br>
2022-07-23 19:07:53 -0300

    Modificando rotas

--- AppConfig/TempoGravidez.js
+++ AppConfig/TempoGravidez.js
@@ -1,14 +1,15 @@
 import React, { useEffect } from 'react';
-import {Text, Image} from 'react-native';
+import {Text, Image, StyleSheet} from 'react-native';
 import { View } from 'react-native';
 import tw from "tailwind-react-native-classnames";
 import twn from '../Tailwind';
 import { useNavigation } from '@react-navigation/native';
 
 
-
 const TempoGravidez = () => {
     const navigation = useNavigation();
+
+    const title = [{t:'periodo', k:1}, {t:'parto', k:2}];
 
     return(
         <View style={twn`flex-1 justify-center w-full p-8 bg-teal-200`} >
@@ -18,22 +19,26 @@ const TempoGravidez = () => {
         />
             <Text
                 style = {twn`text-2xl mt-4 mb-4 text-center font-bold`}>
-                Como você planeja definir o seu tempo de gravidez ?
+                    Como você planeja definir o seu tempo de gravidez ?
             </Text>
             <Text 
-                title = {'período'}
+                title = {title[0].t}
+                key = {title[0].k}
                 style={twn`bg-pink-50 border-2 border-teal-600 text-center text-pink-800 py-2 font-bold text-sm rounded-md mb-2`}
                 onPress={() => navigation.navigate('DataPeriodo')}>
-                Pela data em que meu último período começou
+                    Pela data em que meu último período começou
               </Text>
               <Text 
-                title = {'parto'}
+                title = {title[1].t}
+                key = {title[1].k}
                 style={twn`bg-pink-50 border-2 border-teal-600 text-center text-pink-800 py-2 font-bold text-sm rounded-md`}
                 onPress={() => navigation.navigate('DataParto')}>
-                Pela data de parto estimada
+                    Pela data de parto estimada
               </Text>
         </View>
     );
 }
+
+
 
 export default TempoGravidez;
