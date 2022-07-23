1a258e4d221b561ac8d5fa65fee14b246ac13fd4
nicole-fer <nicolefernandes@alu.ufc.br>
2022-07-23 19:08:40 -0300

    Dgitar a data inicial da ultima menstração

--- AppConfig/DataPeriodo.js
+++ AppConfig/DataPeriodo.js
@@ -1,20 +1,71 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {Text, Image} from 'react-native';
 import { View } from 'react-native';
-import tw from "tailwind-react-native-classnames";
 import twn from '../Tailwind';
+import MaskInput from 'react-native-mask-input';
 import { useNavigation } from '@react-navigation/native';
-import { Button } from 'react-native-web';
 
 const DataPeriodo = () => {
 
-   // const navigation = useNavigation();
+    const [date, setDate] = React.useState('');
+    const navigation = useNavigation();
+
+    const testeOne = (masked) => {
+        setDate(masked)
+        const teste = masked.split('/')
+        const dia = teste[0]
+        const ano = teste[2]
+            if (teste[1] == '01') {
+                teste[1] = 1
+            } else if (teste[0] == '02'){
+                teste[1] = 2
+            } else if (teste[1] == '03'){
+                teste[1] = 3
+            } else if (teste[1] == '04'){
+                teste[1] = 4
+            } else if (teste[1] == '05'){
+                teste[1] = 5
+            } else if (teste[1] == '06'){
+                teste[1] = 6
+            } else if (teste[1] == '07'){
+                teste[1] = 7
+            } else if (teste[1] == '08'){
+                teste[1] = 8
+            } else if (teste[0] == '09'){
+                teste[1] = 9
+            }  
+            const mes = teste[1]
+            console.log('dia',  dia)
+            console.log('mes', mes)
+            console.log('ano', ano)
+    }
+
+    
 
     return(
-        <View style={twn`flex-1 justify-center w-full p-8 bg-teal-200`} >
-            <Text
-                style = {twn`text-2xl mt-4 mb-4 text-center font-bold`}>
-                Data Periodo 
+        <View style={twn`flex-1 justify-center w-full p-8 bg-pink-300`} >
+            <Image
+                style={twn`w-32 h-32 ml-24 mb-8`}
+                source={require('../images/agenda.jpg')}
+            />
+
+            <Text style = {twn`text-lg font-bold text-teal-600`}>
+                Informe a Data do Último Período
+            </Text>
+            <MaskInput
+                style = {twn`text-sm mt-4 mb-4 bg-white text-center p-1 border border-teal-600 rounded-md `}
+                value={date}
+                placeholder={'Digite a data'}
+                onChangeText={testeOne}
+                mask={[/\d/, /\d/, '/' , /\d/, /\d/, '/', /\d/, /\d/]}
+            />
+
+            <Text   
+                style={twn`bg-teal-500 border-2 border-teal-600 text-center text-white py-2 font-bold text-sm rounded-md mt-16 ml-32`}
+                //onPress={() => navigation.navigate('Rota')}
+                //onPress={(testeOne)}
+                > 
+                Próximo
             </Text>
         </View>
     );
