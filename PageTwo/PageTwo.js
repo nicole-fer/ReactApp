fc253813430096a29f1f555e0494f082371f45c8
nicole-fer <nicolefernandes@alu.ufc.br>
2022-08-09 11:07:55 -0300

    Dinamizando text com as infomacoes de Trimestre e semanas e calculando a porcetagem para indicar no progress bar

--- PageTwo/PageTwo.js
+++ PageTwo/PageTwo.js
@@ -9,7 +9,8 @@ import 'moment/locale/pt-br'
 import moment from 'moment';
 import { fb } from '../firebaseconfig';
 import { db } from '../firebaseconfig';
-
+import * as Progress from 'react-native-progress';
+import styled from "styled-components/native";
 
 
 export default function PageTwo ({}){
@@ -35,7 +36,40 @@ export default function PageTwo ({}){
     var diasEspera = Math.ceil(diferenca/(1000*3600*24))
     console.log(diasEspera, 'diasEspera')
 
-  
+    /* Considerando que uma gravidez em media tenha 280 dias, para o cálculo das semanas */
+
+    var semanasDias = 280 - diasEspera
+    var aux=1
+    var cont = 0
+
+    while(aux <= semanasDias){
+        aux = aux + 7
+        cont = cont + 1
+    }
+    aux = aux -1
+    
+    if (aux>semanasDias){
+      var semanas = (aux-7)/7
+      console.log('semanas', semanas)
+      var dataDias = aux-semanasDias
+      console.log(`Você esta com ${semanas} semanas e ${dataDias} dias`)
+      
+    } else if (aux == semanasDias){
+      var semanas = aux/7
+      dataDias = aux-semanasDias
+      console.log(`Você esta com ${semanas} semanas e ${dataDias} dias`)
+    }
+
+    if(semanas <=13){
+        var message = "1º Trimestre"
+    } else if(semanas >=14 && semanas <=26){
+        var message = "2º Trimestre"
+    } else if(semanas >= 27){
+        var message = "3º Trimestre"
+    }
+  /* Calculo da porcentagem */
+  var porcentagem = diasEspera/280
+  console.log(porcentagem)
     return(
         <ScrollView style={twn` w-full bg-white`}> 
             <Text style= {tw`px-4 pt-2 pb-2 mt-2 text-2xl font-bold`} > Hoje </Text>
@@ -51,12 +85,12 @@ export default function PageTwo ({}){
                 />
             </View>
             <View style= {twn`bg-pink-100 px-4 pt-2 pb-2 mb-2 text-center border border-pink-400 mt-4 text-lg ml-4 mr-4 shadow-md rounded-md`} >
-                <Text style= {tw`text-lg mb-2 text-center font-bold`} > 10 Semanas, 6 dias</Text>
-                <Text style= {tw`text-center`} > 1º Trimestre </Text>
+                <Text style= {tw`text-lg mb-2 text-center font-bold text-pink-700`} > {semanas} Semanas, {dataDias} dias</Text>
+                <Text style= {tw`text-center`} > {message}  </Text>
             </View>   
             <View style= {twn`bg-pink-100 px-4 pt-2 pb-2 mb-6 text-center border border-pink-400 mt-2 text-lg ml-4 mr-4 shadow-md rounded-md`} >
-                <Text style= {twn`text-center text-teal-600 mt-2`} > Faltam: {diasEspera} dias </Text>
-                <Text style= {twn`text-center text-teal-600 mt-2`} > ------ </Text> 
+                <Text style= {twn`text-center text-teal-600 mt-2 mb-2`} > Faltam: {diasEspera} dias </Text>
+                <Progress.Bar progress={porcentagem} width={290} color={`#db2777`}/>
                 <Text style= {twn`text-center text-teal-600 mt-2`} > Data Provável do Parto: {firebaseData.dia}/{firebaseData.mes}/{firebaseData.ano} </Text> 
                 {/* Aqui quero botar um componente que vai carregando de acorod com as semanas, quanto mais perto da data, mais preenchido vai ficando */}
             </View>   
