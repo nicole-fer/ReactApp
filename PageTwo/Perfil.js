ca145eebc12c290b7aa135304897c92749b5877a
nicole-fer <nicolefernandes@alu.ufc.br>
2022-08-25 10:08:23 -0300

    Começando a Estilizar a página de Perfil

--- PageTwo/Perfil.js
+++ PageTwo/Perfil.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import {Text, Image} from 'react-native';
-import { SafeAreaView } from 'react-native';
+import React, { useState, useEffect }  from 'react';
+import {Text, Image, TextInput} from 'react-native';
+import { SafeAreaView, ScrollView} from 'react-native';
 import tw from "tailwind-react-native-classnames";
 import twn from '../Tailwind';
-import { fb } from '../firebaseconfig';
+import { fb, db } from '../firebaseconfig';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 
 export default function Perfil ({navigation}){
@@ -19,11 +20,63 @@ export default function Perfil ({navigation}){
           });
     }
 
+    const [firebaseData, setFirebaseData] = useState({})
+    var user = fb.auth().currentUser;    
+
+    useEffect(() => {
+        db.collection('MyCollection').doc(user.uid).get().then(documentSnapshot  => {
+            if (documentSnapshot.exists) {
+              setFirebaseData(documentSnapshot.data())
+            }
+        });
+    },[])
+   console.log(firebaseData)
+
+   const [hidePass, setHidePass] = useState(true);
+   const [myText, setMyText] = useState(true);
+   const [teste, setTeste] = useState('******')
+
+   console.log(myText)
+   const pass = () => {
+        setHidePass(!hidePass)
+        setMyText(!myText)
+         if (myText === true) {
+            //var teste = '******'
+            setTeste(firebaseData.senha)
+        } else if (myText === false) {
+            //var teste = firebaseData.senha
+            setTeste('******')
+        }
+    console.log(myText)
+   }
+    
     return(
-        <SafeAreaView style={tw``} horizontal >
-            <Text style={twn`bg-gray-800`}> Perfil </Text>
-            <Text style={tw`text-blue-700 text-sm font-bold mb-2 mt-4 text-blue-900`}  onPress={(sair)}> Logout</Text> 
-        </SafeAreaView>
+        <ScrollView style={tw`w-full bg-white`} >
+            <Image
+                    style={twn`w-full h-80`}
+                    source={require('../images/fotoPerfil.jpg')}
+            />
+            <Text style={twn`bg-pink-700 text-lg font-bold text-teal-300 pl-4 pt-2 pb-2`}> Conta </Text>
+            <Text style={twn`ml-4 text-teal-500  mt-4`}> Nome </Text>
+            <Text style={twn`border-b border-teal-500 p-1 ml-2 mr-4 pl-4`}> {firebaseData.email} </Text>
+            <Text style={twn`ml-4 text-teal-500 mt-4`}> Email </Text>
+            <Text style={twn`border-b border-teal-500 p-1 ml-2 mr-4 pl-4`}> {firebaseData.email} </Text>
+            <Text style={twn`ml-4 text-teal-500 mt-4`}> Senha </Text>
+            <Text 
+                style={twn`border-b border-teal-500 p-1 ml-2 mr-4 pl-4`}
+                name={myText ? true : false}
+            >
+                {teste}                                                                     -
+                <Icon 
+                    name={hidePass ? 'eye-slash' : 'eye'}
+                    size={15}
+                    color="teal"
+                    style={twn`pl-16`}
+                    onPress={pass}
+                />
+             </Text>
+            <Text style={twn`text-blue-700 text-sm font-bold mb-2 mt-6 text-center border border-teal-500 bg-teal-500 p-2 ml-24 mr-24 rounded-md text-pink-50`}  onPress={(sair)}> Sair da Conta </Text> 
+        </ScrollView>
         
     );
 }
