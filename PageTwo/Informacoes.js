36c49fcce3d4661f43ac69c11c40be1e8aa6d728
nicole-fer <nicolefernandes@alu.ufc.br>
2022-08-29 11:20:26 -0300

    Ajustando novo de estilo de pagina Informações

--- PageTwo/Informacoes.js
+++ PageTwo/Informacoes.js
@@ -8,104 +8,137 @@ import { useNavigation } from '@react-navigation/native';
 
 export default function Informacoes ({navigation}){
     return(
-        <ScrollView style={tw``} >
-            <Text style={twn`bg-white pb-4`}> Informações </Text>
-            <View  style={twn`flex-row bg-pink-100 ml-4 mr-4 border border-pink-300 rounded-xl mt-4`}
+        <ScrollView style={twn`bg-teal-50`} >
+            <View  style={twn`bg-white ml-4 mr-4 border border-teal-800 rounded-xl mt-4`}
              > 
                 <Image                
-                    style={twn`w-12 h-12 ml-4 mt-2`}
-                    source={require('../images/amamentacao.jpg')} 
+                    style={twn`w-full h-48 rounded-t-xl`}
+                    source={require('../images/testee.jpg')} 
                     />
-                     <Text  style={twn`pt-6 ml-2 mb-5 flex-1`}
-                     onPress={() => navigation.navigate('Amamentação')}> Amamentação sem dor </Text>
+                     <Text  style={twn`pt-4 ml-2 mb-2 text-xl font-bold text-center text-pink-900`}
+                     onPress={() => navigation.navigate('Amamentação')}> O que Você Precisa Saber Sobre Amamentação </Text>
+                    <Text  style={twn`ml-4 mr-4 mb-3 text-justify text-gray-500 text-xs`}
+                        onPress={() => navigation.navigate('Amamentação')}>
+                        O seu leite materno é perfeitamente adequado para fornecer os nutrientes que seu bebê precisa, bem como células, hormônios e anticorpos de combate a doenças que podem protegê-lo de doenças.                    </Text>
+                    <Text  style={twn`ml-4 mr-4 mb-4 text-justify text-teal-500 text-xs`}
+                     onPress={() => navigation.navigate('Amamentação')}>Clique para saber mais</Text>
             </View>
-            <View  style={twn`flex-row bg-pink-100 ml-4 mr-4 border border-pink-300 rounded-xl mt-4`}> 
+            <View  style={twn`bg-white ml-4 mr-4 border border-teal-800 rounded-xl mt-4`}
+             >
                 <Image                
-                    style={twn`w-12 h-12 ml-4 mt-2`}
-                    source={require('../images/agenda.jpg')} 
-                    
+                    style={twn`w-full h-48 rounded-t-xl`}
+                    source={require('../images/artigo2.jpg')} 
                     />
-                     <Text  style={twn`pt-6 ml-2 mb-5 flex-1`}> Cansada? Como lidar com a fadiga na gravidez ? </Text>
+                     <Text  style={twn`pt-4 ml-2 mb-2 text-xl font-bold text-center text-pink-900`}
+                     onPress={() => navigation.navigate('Amamentação')}> Artigo Dois </Text>
+                    <Text  style={twn`ml-4 mr-4 mb-3 text-justify text-gray-500 text-xs`}
+                     onPress={() => navigation.navigate('Amamentação')}>dwdwdwd ijdqwjdqwd dwjqwdjwqdnqw  dwdwdwd ijdqwjdqwd dwjqwdjwqdnqw </Text>
+                    <Text  style={twn`ml-4 mr-4 mb-4 text-justify text-teal-500 text-xs`}
+                     onPress={() => navigation.navigate('Amamentação')}>Clique para saber mais</Text>
             </View>
-            <View  style={twn`flex-row bg-pink-100 ml-4 mr-4 border border-pink-300 rounded-xl mt-4`}> 
+            <View  style={twn`bg-white ml-4 mr-4 border border-teal-800 rounded-xl mt-4`}
+             > 
                 <Image                
-                    style={twn`w-12 h-12 ml-4 mt-2`}
-                    source={require('../images/agenda.jpg')} 
-                    
+                    style={twn`w-full h-48 rounded-t-xl`}
+                    source={require('../images/artigo3.jpg')} 
                     />
-                     <Text  style={twn`pt-6 ml-2 mb-5 flex-1`}> Amamentação sem dor </Text>
+                     <Text  style={twn`pt-4 ml-2 mb-2 text-xl font-bold text-center text-pink-900`}
+                     onPress={() => navigation.navigate('Amamentação')}> Artigo Três </Text>
+                    <Text  style={twn`ml-4 mr-4 mb-3 text-justify text-gray-500 text-xs`}
+                     onPress={() => navigation.navigate('Amamentação')}>dwdwdwd ijdqwjdqwd dwjqwdjwqdnqw  dwdwdwd ijdqwjdqwd dwjqwdjwqdnqw </Text>
+                    <Text  style={twn`ml-4 mr-4 mb-4 text-justify text-teal-500 text-xs`}
+                     onPress={() => navigation.navigate('Amamentação')}>Clique para saber mais</Text>
             </View>
-            <View  style={twn`flex-row bg-pink-100 ml-4 mr-4 border border-pink-300 rounded-xl mt-4`}> 
+            <View  style={twn`bg-white ml-4 mr-4 border border-teal-800 rounded-xl mt-4`}
+             > 
                 <Image                
-                    style={twn`w-12 h-12 ml-4 mt-2`}
-                    source={require('../images/agenda.jpg')} 
-                    
+                    style={twn`w-full h-48 rounded-t-xl`}
+                    source={require('../images/artigo6.jpg')} 
                     />
-                     <Text  style={twn`pt-6 ml-2 mb-5 flex-1`}> Amamentação sem dor </Text>
+                     <Text  style={twn`pt-4 ml-2 mb-2 text-xl font-bold text-center text-pink-900`}
+                     onPress={() => navigation.navigate('Amamentação')}> Artigo Quatro </Text>
+                    <Text  style={twn`ml-4 mr-4 mb-3 text-justify text-gray-500 text-xs`}
+                     onPress={() => navigation.navigate('Amamentação')}>dwdwdwd ijdqwjdqwd dwjqwdjwqdnqw  dwdwdwd ijdqwjdqwd dwjqwdjwqdnqw </Text>
+                    <Text  style={twn`ml-4 mr-4 mb-4 text-justify text-teal-500 text-xs`}
+                     onPress={() => navigation.navigate('Amamentação')}>Clique para saber mais</Text>
             </View>
-            <View  style={twn`flex-row bg-pink-100 ml-4 mr-4 border border-pink-300 rounded-xl mt-4`}> 
+            <View  style={twn`bg-white ml-4 mr-4 border border-teal-800 rounded-xl mt-4`}
+             >
                 <Image                
-                    style={twn`w-12 h-12 ml-4 mt-2`}
-                    source={require('../images/agenda.jpg')} 
-                    
+                    style={twn`w-full h-48 rounded-t-xl`}
+                    source={require('../images/artigo4.jpg')}
                     />
-                     <Text  style={twn`pt-6 ml-2 mb-5 flex-1`}> Cansada? Como lidar com a fadiga na gravidez ? </Text>
+                     <Text  style={twn`pt-4 ml-2 mb-2 text-xl font-bold text-center text-pink-900`}
+                     onPress={() => navigation.navigate('Amamentação')}> Artigo Cinco </Text>
+                    <Text  style={twn`ml-4 mr-4 mb-3 text-justify text-gray-500 text-xs`}
+                     onPress={() => navigation.navigate('Amamentação')}>dwdwdwd ijdqwjdqwd dwjqwdjwqdnqw  dwdwdwd ijdqwjdqwd dwjqwdjwqdnqw </Text>
+                    <Text  style={twn`ml-4 mr-4 mb-4 text-justify text-teal-500 text-xs`}
+                     onPress={() => navigation.navigate('Amamentação')}>Clique para saber mais</Text>
             </View>
-            <View  style={twn`flex-row bg-pink-100 ml-4 mr-4 border border-pink-300 rounded-xl mt-4`}> 
+            <View  style={twn`bg-white ml-4 mr-4 border border-teal-800 rounded-xl mt-4`}
+             > 
                 <Image                
-                    style={twn`w-12 h-12 ml-4 mt-2`}
-                    source={require('../images/agenda.jpg')} 
-                    
+                    style={twn`w-full h-48 rounded-t-xl`}
+                    source={require('../images/artigo5.jpg')} 
                     />
-                     <Text  style={twn`pt-6 ml-2 mb-5 flex-1`}> Amamentação sem dor </Text>
+                     <Text  style={twn`pt-4 ml-2 mb-2 text-xl font-bold text-center text-pink-900`}
+                     onPress={() => navigation.navigate('Amamentação')}> Artigo Seis </Text>
+                    <Text  style={twn`ml-4 mr-4 mb-3 text-justify text-gray-500 text-xs`}
+                     onPress={() => navigation.navigate('Amamentação')}>dwdwdwd ijdqwjdqwd dwjqwdjwqdnqw  dwdwdwd ijdqwjdqwd dwjqwdjwqdnqw </Text>
+                    <Text  style={twn`ml-4 mr-4 mb-4 text-justify text-teal-500 text-xs`}
+                     onPress={() => navigation.navigate('Amamentação')}>Clique para saber mais</Text>
             </View>
-            <View  style={twn`flex-row bg-pink-100 ml-4 mr-4 border border-pink-300 rounded-xl mt-4`}> 
+            <View  style={twn`bg-white ml-4 mr-4 border border-teal-800 rounded-xl mt-4`}
+             > 
                 <Image                
-                    style={twn`w-12 h-12 ml-4 mt-2`}
-                    source={require('../images/agenda.jpg')} 
-                    
+                    style={twn`w-full h-48 rounded-t-xl`}
+                    source={require('../images/artigo7.jpg')} 
                     />
-                     <Text  style={twn`pt-6 ml-2 mb-5 flex-1`}> Amamentação sem dor </Text>
+                     <Text  style={twn`pt-4 ml-2 mb-2 text-xl font-bold text-center text-pink-900`}
+                     onPress={() => navigation.navigate('Amamentação')}> Artigo Sete </Text>
+                    <Text  style={twn`ml-4 mr-4 mb-3 text-justify text-gray-500 text-xs`}
+                     onPress={() => navigation.navigate('Amamentação')}>dwdwdwd ijdqwjdqwd dwjqwdjwqdnqw  dwdwdwd ijdqwjdqwd dwjqwdjwqdnqw </Text>
+                    <Text  style={twn`ml-4 mr-4 mb-4 text-justify text-teal-500 text-xs`}
+                     onPress={() => navigation.navigate('Amamentação')}>Clique para saber mais</Text>
             </View>
-            <View  style={twn`flex-row bg-pink-100 ml-4 mr-4 border border-pink-300 rounded-xl mt-4`}> 
+            <View  style={twn`bg-white ml-4 mr-4 border border-teal-800 rounded-xl mt-4`}
+             > 
                 <Image                
-                    style={twn`w-12 h-12 ml-4 mt-2`}
-                    source={require('../images/agenda.jpg')} 
-                    
+                    style={twn`w-full h-48 rounded-t-xl`}
+                    source={require('../images/artigo8.jpg')} 
                     />
-                     <Text  style={twn`pt-6 ml-2 mb-5 flex-1`}> Cansada? Como lidar com a fadiga na gravidez ? </Text>
+                     <Text  style={twn`pt-4 ml-2 mb-2 text-xl font-bold text-center text-pink-900`}
+                     onPress={() => navigation.navigate('Amamentação')}> Artigo Oito </Text>
+                    <Text  style={twn`ml-4 mr-4 mb-3 text-justify text-gray-500 text-xs`}
+                     onPress={() => navigation.navigate('Amamentação')}>dwdwdwd ijdqwjdqwd dwjqwdjwqdnqw  dwdwdwd ijdqwjdqwd dwjqwdjwqdnqw </Text>
+                    <Text  style={twn`ml-4 mr-4 mb-4 text-justify text-teal-500 text-xs`}
+                     onPress={() => navigation.navigate('Amamentação')}>Clique para saber mais</Text>
             </View>
-            <View  style={twn`flex-row bg-pink-100 ml-4 mr-4 border border-pink-300 rounded-xl mt-4`}> 
+            <View  style={twn`bg-white ml-4 mr-4 border border-teal-800 rounded-xl mt-4`}
+             > 
                 <Image                
-                    style={twn`w-12 h-12 ml-4 mt-2`}
-                    source={require('../images/agenda.jpg')} 
-                    
+                    style={twn`w-full h-48 rounded-t-xl`}
+                    source={require('../images/artigo9.jpg')} 
                     />
-                     <Text  style={twn`pt-6 ml-2 mb-5 flex-1`}> Amamentação sem dor </Text>
+                     <Text  style={twn`pt-4 ml-2 mb-2 text-xl font-bold text-center text-pink-900`}
+                     onPress={() => navigation.navigate('Amamentação')}> Artigo Nove </Text>
+                    <Text  style={twn`ml-4 mr-4 mb-3 text-justify text-gray-500 text-xs`}
+                     onPress={() => navigation.navigate('Amamentação')}>dwdwdwd ijdqwjdqwd dwjqwdjwqdnqw  dwdwdwd ijdqwjdqwd dwjqwdjwqdnqw </Text>
+                    <Text  style={twn`ml-4 mr-4 mb-4 text-justify text-teal-500 text-xs`}
+                     onPress={() => navigation.navigate('Amamentação')}>Clique para saber mais</Text>
             </View>
-            <View  style={twn`flex-row bg-pink-100 ml-4 mr-4 border border-pink-300 rounded-xl mt-4`}> 
+            <View  style={twn`bg-white ml-4 mr-4 border border-teal-800 rounded-xl mt-4`}
+             > 
                 <Image                
-                    style={twn`w-12 h-12 ml-4 mt-2`}
-                    source={require('../images/agenda.jpg')} 
-                    
+                    style={twn`w-full h-48 rounded-t-xl`}
+                    source={require('../images/artigo10.jpg')} 
                     />
-                     <Text  style={twn`pt-6 ml-2 mb-5 flex-1`}> Amamentação sem dor </Text>
-            </View>
-            <View  style={twn`flex-row bg-pink-100 ml-4 mr-4 border border-pink-300 rounded-xl mt-4`}> 
-                <Image                
-                    style={twn`w-12 h-12 ml-4 mt-2`}
-                    source={require('../images/agenda.jpg')} 
-                    
-                    />
-                     <Text  style={twn`pt-6 ml-2 mb-5 flex-1`}> Cansada? Como lidar com a fadiga na gravidez ? </Text>
-            </View>
-            <View  style={twn`flex-row bg-pink-100 ml-4 mr-4 border border-pink-300 rounded-xl mt-4`}> 
-                <Image                
-                    style={twn`w-12 h-12 ml-4 mt-2`}
-                    source={require('../images/agenda.jpg')} 
-                    
-                    />
-                     <Text  style={twn`pt-6 ml-2 mb-5 flex-1`}> Amamentação sem dor </Text>
+                     <Text  style={twn`pt-4 ml-2 mb-2 text-xl font-bold text-center text-pink-900`}
+                     onPress={() => navigation.navigate('Amamentação')}> Artigo Dez </Text>
+                    <Text  style={twn`ml-4 mr-4 mb-3 text-justify text-gray-500 text-xs`}
+                     onPress={() => navigation.navigate('Amamentação')}>dwdwdwd ijdqwjdqwd dwjqwdjwqdnqw  dwdwdwd ijdqwjdqwd dwjqwdjwqdnqw </Text>
+                    <Text  style={twn`ml-4 mr-4 mb-4 text-justify text-teal-500 text-xs`}
+                     onPress={() => navigation.navigate('Amamentação')}>Clique para saber mais</Text>
             </View>
          
             
