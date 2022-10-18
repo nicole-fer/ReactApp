9b8d521e9788e2f4ac489d8507a761505e35a59c
nicole-fer <nicolefernandes@alu.ufc.br>
2022-10-18 15:09:25 -0300

    Adicionando onPress para ver lista de tarefas

--- PageTwo/AgendaComponent.js
+++ PageTwo/AgendaComponent.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import {Text, Image} from 'react-native';
+import {Text, Image, View} from 'react-native';
 import { SafeAreaView, ScrollView } from 'react-native';
 import tw from "tailwind-react-native-classnames";
 import twn from '../Tailwind';
 import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 import {LocaleConfig} from 'react-native-calendars';
+import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
+import { AntDesign } from '@expo/vector-icons'
 
 
 LocaleConfig.locales['fr'] = {
@@ -29,7 +31,7 @@ LocaleConfig.locales['fr'] = {
   };
   LocaleConfig.defaultLocale = 'fr';
 
-export default function AgendaComponent ({}){
+export default function AgendaComponent ({ navigation }){
     
     return(
         <ScrollView style={tw`w-full bg-white`} >
@@ -49,6 +51,21 @@ export default function AgendaComponent ({}){
                     console.log('selected day', day);
                   }}
                 />
+
+          <View  style={twn`pt-12 pl-2`}> 
+              <FlatList />
+              <View style={twn`flex-row bg-pink-700 rounded-lg mr-4 ml-2`} >
+                <Text  
+                  numberOfLines={2} 
+                  style={twn`pb-4 pl-2 font-bold text-lg flex-1 ml-2 pt-2 text-teal-300`}  
+                  onPress={() => navigation.navigate('Tarefas')} >
+                    Ver Tarefas 
+                </Text>
+                <TouchableOpacity style={twn`pl-4 pr-4 pt-2`} onPress={() => navigation.navigate('Tarefas')} >
+                <AntDesign name="filetext1" size={30} color={'#5eead4'} />
+                </TouchableOpacity>
+              </View>
+            </View>
         </ScrollView>
     );
 }
